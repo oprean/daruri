588e81ba01a7a13a34c6ca85aad688548068b702
Oprean Sergiu <oprean@gmail.com>
2015-05-24 10:22:31 +0300

    added icons and hidden clear local storage

--- js/views/FooterView.js
+++ js/views/FooterView.js
@@ -7,6 +7,14 @@ define([
 ], function($, _, Backbone, Marionette, footerTpl){
 	var FooterView = Backbone.Marionette.ItemView.extend({
 		template : _.template(footerTpl),
+		events : {
+			'click .local-storage-clear' : 'localStorageClear'
+		},
+		
+		localStorageClear : function() {
+			localStorage.clear();
+			location.reload(true);
+		}
 	});
 	 
 	return FooterView;
