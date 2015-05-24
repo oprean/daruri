0cb5f4ec5466beebddd7c60cf05314275a8f2fcc
Oprean Sergiu <oprean@gmail.com>
2015-05-24 22:07:17 +0300

    switched to long quiz

--- js/modules/Constants.js
+++ js/modules/Constants.js
@@ -1,8 +1,8 @@
 define([
 ], function(){
 	var Constants = {
-		DEFAULT_QUIZ : 'test',
-		//DEFAULT_QUIZ : 'gifts',
+		//DEFAULT_QUIZ : 'test',
+		DEFAULT_QUIZ : 'gifts',
 		PARSE : {
 			AppID : '0yXFaABnkqXVSBDvY11WIRia0vWEVDnTCc959zTy',
 			JsKey : 'DpKYEptsNFQD9Hz7trcvl7bNTdtTcfGqZdG6EGas'
