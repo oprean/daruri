c7da04bc0755a381290278fa9e43ffef84561f21
unknown <sergiu.oprean@SERGIU-PC.bitalb.ro>
2015-05-25 15:12:49 +0300

    preparing love.json

--- js/modules/Constants.js
+++ js/modules/Constants.js
@@ -4,6 +4,7 @@ define([
 		//DEFAULT_QUIZ : 'test',
 		//DEFAULT_QUIZ : 'gifts',
 		DEFAULT_QUIZ : 'conflict',
+		//DEFAULT_QUIZ : 'love',
 		PARSE : {
 			AppID : '0yXFaABnkqXVSBDvY11WIRia0vWEVDnTCc959zTy',
 			JsKey : 'DpKYEptsNFQD9Hz7trcvl7bNTdtTcfGqZdG6EGas'
