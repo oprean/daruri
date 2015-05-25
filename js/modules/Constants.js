d1974283996d74d1777de959ea9f130cd1260d7e
unknown <sergiu.oprean@SERGIU-PC.bitalb.ro>
2015-05-25 13:47:42 +0300

    added conflict quiz

--- js/modules/Constants.js
+++ js/modules/Constants.js
@@ -2,7 +2,8 @@ define([
 ], function(){
 	var Constants = {
 		//DEFAULT_QUIZ : 'test',
-		DEFAULT_QUIZ : 'gifts',
+		//DEFAULT_QUIZ : 'gifts',
+		DEFAULT_QUIZ : 'conflict',
 		PARSE : {
 			AppID : '0yXFaABnkqXVSBDvY11WIRia0vWEVDnTCc959zTy',
 			JsKey : 'DpKYEptsNFQD9Hz7trcvl7bNTdtTcfGqZdG6EGas'
