a2c5e7d5a7847edc07b4ffe2755d7352e16e4c85
unknown <sergiu.oprean@SERGIU-PC.bitalb.ro>
2015-05-26 12:58:05 +0300

    added app DEBUG mode

--- js/models/QuizzesHome.js
+++ js/models/QuizzesHome.js
@@ -2,8 +2,16 @@ define([
   'jquery',
   'underscore',
   'backbone',
-], function($, _, Backbone){
+  'modules/Constants'
+], function($, _, Backbone, Constants){
 	var QuizzesHome = Backbone.Model.extend({
+		initialize: function(options) {
+			if (Constants.DEBUG == false) {
+				options.quizzes = 
+				options.quizzes.slice(0, options.quizzes.length-2); 
+			}
+			this.set(options);
+		}
 	});
 
 	return QuizzesHome;
--- js/modules/Constants.js
+++ js/modules/Constants.js
@@ -1,10 +1,8 @@
 define([
 ], function(){
 	var Constants = {
-		//DEFAULT_QUIZ : 'test',
-		//DEFAULT_QUIZ : 'gifts',
+		DEBUG : false,
 		DEFAULT_QUIZ : 'conflict',
-		//DEFAULT_QUIZ : 'love',
 		PARSE : {
 			AppID : '0yXFaABnkqXVSBDvY11WIRia0vWEVDnTCc959zTy',
 			JsKey : 'DpKYEptsNFQD9Hz7trcvl7bNTdtTcfGqZdG6EGas'
