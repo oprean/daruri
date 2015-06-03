4c6aca2a79382c830df24a7482266381e555cad9
Oprean Sergiu <oprean@gmail.com>
2015-06-03 20:07:32 +0300

    extract sensitive data from mail.php

--- php/mail.php
+++ php/mail.php
@@ -1,18 +1,20 @@
 <?php
+require_once('config.php');
 require_once('phpmailer/class.smtp.php');
 require_once('phpmailer/class.phpmailer.php');
 
 if(isset($_POST['mail'])) {
 	$mail = new PHPMailer(); // create a new object
+	$mail->CharSet = 'UTF-8';
 	$mail->IsSMTP(); // enable SMTP
 	$mail->SMTPAuth = true; // authentication enabled
 	$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
 	$mail->Host = "smtp.gmail.com";
 	$mail->Port = 465; // or 587
 	$mail->IsHTML(true);
-	$mail->Username = "";
-	$mail->Password = "";
-	$mail->SetFrom("");
+	$mail->Username = SMTP_USERNAME;
+	$mail->Password = SMTP_PASSWORD;
+	$mail->SetFrom(SMTP_SETFROM);
 	$mail->Subject = $_POST['mail']['subject'];
 	$mail->Body = $_POST['mail']['body'];
 	$mail->AddAddress($_POST['mail']['email']);
