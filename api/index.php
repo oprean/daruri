40aed4f50e2e207ada44534a6d4d0fbfa3d30a48
unknown <sergiu.oprean@SERGIU-PC.bitalb.ro>
2015-06-12 17:01:45 +0300

    start results api implementation

--- api/index.php
+++ api/index.php
@@ -6,57 +6,116 @@ R::setup( 'sqlite:../assets/data/quizzes.sqlite' );
 
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
-//http://stackoverflow.com/questions/7017755/sub-views-layouts-templates-in-slim-php-framework
 
-// GET route
-$app->get(
-    '/',
-    function () {
-    	$book = R::dispense( 'book' );
-		$book->title = 'Learn to Program';
-    	$book->rating = 10;
-		$id = R::store( $book );
-        $template = 'html';
-        echo $template;
-    }
-);
-
-$app->get(
-    '/quiz',
-    function () {
-        $template = 'gallery';
-        echo $template;
-    }
-);
-
-// POST route
-$app->post(
-    '/post',
-    function () {
-        echo 'This is a POST route';
-    }
-);
-
-// PUT route
-$app->put(
-    '/put',
-    function () {
-        echo 'This is a PUT route';
-    }
-);
-
-// PATCH route
-$app->patch('/patch', function () {
-    echo 'This is a PATCH route';
-});
-
-// DELETE route
-$app->delete(
-    '/delete',
-    function () {
-        echo 'This is a DELETE route';
-    }
-);
-
+// handle GET requests for /api/items
+$app->get('/results', 'getResults');
+$app->get('/results/:id',  'getResult');
+ 
+// handle POST requests
+$app->post('/results', 'addResult');
+ 
+// handle PUT requests
+$app->put('/results/:id', 'updateResult');
+ 
+// handle DELETE requests
+$app->delete('/results/:id', 'deleteResult');
+ 
 $app->run();
 R::close();
+
+function getResults() {
+    // get all items
+    $items = R::find('results');
+     
+    $app = \Slim\Slim::getInstance();
+     
+    // create JSON response
+    $app->response()->header('Content-Type', 'application/json');
+    echo json_encode(R::exportAll($items));
+}
+ 
+function getResult($id) {
+ 
+    $items = R::findOne('results', 'id=?', array($id));
+     
+    // create JSON response if element found 
+    // else send 404 server error
+    $app = \Slim\Slim::getInstance();
+    if ($items) {
+        $app->response()->header('Content-Type', 'application/json');
+        echo json_encode(R::exportAll($items));
+    } else {
+        $app->response()->status(404);
+    }
+}
+ 
+function addResult() {
+ 
+    $app = \Slim\Slim::getInstance();
+     
+    // get request body, decode into PHP object
+     
+    $request = $app->request();
+    $body = $request->getBody(); 
+    $input = json_decode($body);
+ 
+    // create and save element record 
+    $items = R::dispense('results'); 
+    $items->quizId = (string)$input->quiz_id; 
+	$items->person = (string)$input->person;
+	$items->date = date('Y-m-d');
+	$items->answers = (string)$input->answers;
+     
+    // do same for other attributes 
+    R::store($items); 
+     
+    // create and send JSON response 
+    $app->response()->status(201); 
+    $app->response()->header('Content-Type', 'application/json');
+    echo json_encode(R::exportAll($items));
+     
+}
+ 
+function updateResult($id) {
+     
+    $app = \Slim\Slim::getInstance();
+     
+    // get request body, decode into PHP object
+    $request = $app->request();
+     
+    $body = $request->getBody();
+    $input = json_decode($body);
+     
+ 
+    // retrieve specified element record 
+    // save modified record, create and send JSON response 
+    $items = R::findOne('results', 'id = ?', array($id)); 
+     
+    if ($items) { 
+        $items->quizId = (string)$input->quiz_id; 
+        // do same for other attributes 
+        R::store($items); 
+        $app->response()->header('Content-Type', 'application/json');
+        echo json_encode(R::exportAll($items)); 
+    } else { 
+        $app->response()->status(404); 
+    }
+     
+}
+ 
+function deleteResult($id) {
+     
+    $app = \Slim\Slim::getInstance();
+     
+    // retrieve specified element record 
+    // save modified record, create and send JSON response 
+    $items = R::findOne('results', 'id = ?', array($id)); 
+     
+    if ($items) { 
+        R::trash($items); 
+        $app->response()->status(204); 
+    } else { 
+        $app->response()->status(404); 
+    }
+     
+}
