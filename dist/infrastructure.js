8917a22504bb3eae7c0abaae63855217ec87be8c
unknown <sergiu.oprean@SERGIU-PC.bitalb.ro>
2015-08-20 10:45:02 +0300

    hide generate pdf & send email until we finish

--- dist/infrastructure.js
+++ dist/infrastructure.js
@@ -1,1 +1,1 @@
-define(["jquery","jquery.bootstrap","jquery.ui","jquery.ui.touch-punch","jquery.ui.custom","underscore","backbone","backbone.marionette","backbone.localStorage","backbone.validation","moment","jquery.qrcode","text"],function(){});
+define(["jquery","jquery.bootstrap","jquery.ui","jquery.ui.touch-punch","jquery.ui.custom","underscore","backbone","backbone.marionette","backbone.localStorage","backbone.validation","backgrid","moment","jquery.qrcode","text"],function(){});
